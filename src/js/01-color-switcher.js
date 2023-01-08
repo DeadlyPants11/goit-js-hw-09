31d92b00afa90cec985054be28413ea3a4c60938
DeadlyPants11 <ostrjanin.rostik@gmail.com>
2023-01-08 21:29:40 +0200

    done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const changeBG = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    let color = getRandomHexColor();
+    changeBG.style.backgroundColor = color;
+  }, 1000);
+  startBtn.setAttribute('disabled', 'disabled');
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled', 'disabled');
+});
